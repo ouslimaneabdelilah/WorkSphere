9db24c564968ddb46abddd4bd35a54ad26870d5d
abdelilah ouslimane <abdelilah.ouslimane3740@gmail.com>
2025-11-17 12:13:56 +0100

    feat: Ajouter Modal et events pour close modal

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
-const url = "./employees.json"
-let employees = []
-document.addEventListener('DOMContentLoaded', function () {
-	const STORAGE_KEY = "employees"
-	async function loadData(){
-		try {
-			const stored = localStorage.getItem(STORAGE_KEY)
-			if (stored) {
-				employees = JSON.parse(stored)
-				return employees
-			}
-			const res = await fetch(url)
-			const data = await res.json()
-			employees = Array.isArray(data) ? data : []
-			localStorage.setItem(STORAGE_KEY, JSON.stringify(employees))
-			return employees
-		} catch (err) {
-			console.error('probleme pour loading data:', err)
-		}
-	}
+document.addEventListener("DOMContentLoaded", function () {
+  const STORAGE_KEY = "employees";
+  const url = "./employees.json";
+  const closeModal = document.querySelector(".close-modal")
+  const btnAdd = document.getElementById("btn-add")
+  let employees = [];
+  // function pour loadData
+  async function loadData() {
+    try {
+      const stored = localStorage.getItem(STORAGE_KEY);
+      if (stored) {
+        employees = JSON.parse(stored);
+        return employees;
+      }
+      const res = await fetch(url);
+      const data = await res.json();
+      employees = Array.isArray(data) ? data : [];
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(employees));
+      return employees;
+    } catch (err) {
+      console.error("probleme pour loading data:", err);
+    }
+  }
+  // function pour saveData
 
-	function saveData(){
-		try {
-			localStorage.setItem(STORAGE_KEY, JSON.stringify(employees))
-		} catch (err) {
-			console.error('probleme pour save data:', err)
-		}
-	}
+  function saveData() {
+    try {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(employees));
+    } catch (err) {
+      console.error("probleme pour save data:", err);
+    }
+  }
 
-})
+  // event pour close modal
+  closeModal.addEventListener('click',()=>{
+	document.getElementById("dialog").classList.add('is-hidden');
+  })
+  // Event Click Add New Worker
+  btnAdd.addEventListener("click",()=>{
+	document.getElementById("dialog").classList.remove('is-hidden');
+  })
+
+
+
+});
